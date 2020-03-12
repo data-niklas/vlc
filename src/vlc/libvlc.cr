c7c0dc3990e5ca6618a85dfefdae2506d09b8215
Niklas Loeser <niklas@4loeser.net>
2020-03-12 12:22:58 +0100

    adds equalizer and audio settings

--- src/vlc/libvlc.cr
+++ src/vlc/libvlc.cr
@@ -10,6 +10,7 @@ lib LibVlc
     alias VlcEventType = LibC::Int
     alias VlcCallback = Proc(VlcEventData*, Void*, Nil)
     alias VlcPicture = Void*
+    alias VlcEqualizer = Void*
 
     enum VlcState
         NothingSpecial
@@ -139,6 +140,18 @@ lib LibVlc
         Jpg
     end
 
+    enum VlcRole
+        None
+        Music
+        Video
+        Communication
+        Game
+        Notification
+        Animation
+        Production
+        Accessibility
+    end
+
 
     struct VlcMediaStats
         i_read_bytes : LibC::Int
@@ -185,17 +198,78 @@ lib LibVlc
         device : Char*
     end
 
+    struct VlcAudioOutput
+        psz_name : Char*
+        psz_description : Char*
+        p_next : VlcAudioOutput*
+    end
+
+    struct VlcTrackDescription
+        i_id : LibC::Int
+        psz_name : Char*
+        p_next : VlcTrackDescription*
+    end
+
+    struct VlcOutputDevice
+        psz_device : Char*
+        psz_description : Char*
+        p_next : VlcOutputDevice*
+    end
+
     # Main and instance
     fun new_instance = libvlc_new(arguments_count: LibC::Int, arguments: LibC::Char*) : VlcInstance*
     fun free = libvlc_free(Void*)
     fun release_instance = libvlc_release(instance: VlcInstance*)
     fun version = libvlc_get_version(): LibC::Char*
     fun compiler_version = libvlc_get_compiler(): LibC::Char*
+    
+    fun get_audio_outputs = libvlc_audio_output_list_get(instance : VlcInstance*) : VlcAudioOutput*
+    fun free_audio_outputs = libvlc_audio_output_list_release(outputs : VlcAudioOutput*) 
+    fun set_audio_output = libvlc_audio_output_set(media_player : VlcMediaPlayer*, psz_name : Char*) : LibC::Int
+    
+    fun set_audio_channel = libvlc_audio_set_channel(media_player : VlcMediaPlayer*, channel : LibC::Int) : LibC::Int
+    fun set_audio_delay = libvlc_audio_set_delay(media_player : VlcMediaPlayer*, delay : LibC::Int64T) : LibC::Int # delay in microseconds
+    fun set_audio_mute = libvlc_audio_set_mute(media_player : VlcMediaPlayer*, status : LibC::Int) # Not reliable!
+    fun set_audio_track = libvlc_audio_set_track(media_player : VlcMediaPlayer*, track : LibC::Int) : LibC::Int
+    fun set_audio_volume = libvlc_audio_set_volume(media_player : VlcMediaPlayer*, volume : LibC::Int) : LibC::Int#volume in percent --> between 0 and 100 (inclusive)
+    
+    fun get_audio_channel = libvlc_audio_get_channel(media_player : VlcMediaPlayer*) : LibC::Int
+    fun get_audio_delay = libvlc_audio_get_delay(media_player : VlcMediaPlayer*) : LibC::Int64T
+    fun get_audio_mute = libvlc_audio_get_mute(media_player : VlcMediaPlayer*) : LibC::Int
+    fun get_audio_track = libvlc_audio_get_track(media_player : VlcMediaPlayer*) : LibC::Int
+    fun get_audio_track_count = libvlc_audio_get_track_count(media_player : VlcMediaPlayer*) : LibC::Int
+    fun get_audio_volume = libvlc_audio_get_volume(media_player : VlcMediaPlayer*) : LibC::Int
+    fun enum_audio_output_devices = libvlc_audio_output_device_enum(media_player : VlcMediaPlayer*) : VlcOutputDevice*
+    fun get_audio_output_device = libvlc_audio_output_device_get(media_player : VlcMediaPlayer*) : Char*
+    fun get_audio_output_devices = libvlc_audio_output_device_list_get(instance : VlcInstance*, audio_out : Char*) : VlcOutputDevice*
+    fun free_audio_output_device = libvlc_audio_output_device_list_release(device : VlcOutputDevice*)
+    fun set_audio_output_device = libvlc_audio_output_device_set(media_player : VlcMediaPlayer*, audio_output_module : Char*, device_id : Char*)
+
+    fun get_audio_track_description = libvlc_audio_get_track_description(media_player : VlcMediaPlayer*) : VlcTrackDescription*
+
+    fun toggle_audio_mute = libvlc_audio_toggle_mute(media_player : VlcMediaPlayer*)
+    fun get_media_player_role = libvlc_media_player_get_role(media_player : VlcMediaPlayer*) : VlcRole
+    fun set_media_player_role = libvlc_media_player_set_role(media_player : VlcMediaPlayer*, role : VlcRole) : LibC::Int
+    fun set_media_player_equalizer = libvlc_media_player_set_equalizer(media_player : VlcMediaPlayer*, equalizer : VlcEqualizer*) : LibC::Int
+
+    # Equalizer
+    fun get_equalizer_amp_at_index = libvlc_audio_equalizer_get_amp_at_index(equalizer : VlcEqualizer*, band : LibC::Int) : LibC::Float
+    fun get_equalizer_band_count = libvlc_audio_equalizer_get_band_count() : LibC::Int
+    fun get_equalizer_band_frequency = libvlc_audio_equalizer_get_band_frequency(index : LibC::Int) : LibC::Float
+    fun get_equalizer_preamp = libvlc_audio_equalizer_get_preamp(equalizer : VlcEqualizer*) : LibC::Float
+    fun get_equalizer_preset_count = libvlc_audio_equalizer_get_preset_count() : LibC::Int
+    fun get_equalizer_preset_name = libvlc_audio_equalizer_get_preset_name(index : LibC::Int) : Char*
+    
+    fun set_equalizer_amp_at_index = libvlc_audio_equalizer_set_amp_at_index(equalizer : VlcEqualizer*, amp : LibC::Float, band : LibC::Int) : LibC::Int # Float needs to be between -20 and 20 (inclusive)
+    fun set_equalizer_preamp = libvlc_audio_equalizer_set_preamp(equalizer : VlcEqualizer*, preamp : LibC::Float) : LibC::Int
+    fun new_equalizer = libvlc_audio_equalizer_new() : VlcEqualizer*
+    fun new_equalizer_from_preset = libvlc_audio_equalizer_new_from_preset(index : LibC::Int) : VlcEqualizer*
+    fun free_equalizer = libvlc_audio_equalizer_release(equalizer : VlcEqualizer*)
 
     # Media
     fun new_media_from_path = libvlc_media_new_path(instance : VlcInstance*, path : LibC::Char*) : VlcMedia*
     fun new_media_from_location = libvlc_media_new_location(instance : VlcInstance*, path : LibC::Char*) : VlcMedia*
-    fun release_media = libvlc_media_release(media : VlcMedia*)
+    fun free_media = libvlc_media_release(media : VlcMedia*)
     fun get_media_resource_locator = libvlc_media_get_mrl(media : VlcMedia*) : LibC::Char*
     fun get_media_meta = libvlc_media_get_meta(media : VlcMedia*, meta : VlcMeta) : LibC::Char*
     fun get_media_duration = libvlc_media_get_duration(media : VlcMedia*) : VlcTime
@@ -207,7 +281,7 @@ lib LibVlc
     # Media player
     fun new_media_player = libvlc_media_player_new(instance : VlcInstance*) : VlcMediaPlayer*
     fun new_media_player_from_media = libvlc_media_player_new_from_media(media : VlcMedia*) : VlcMediaPlayer*
-    fun release_media_player = libvlc_media_player_release(media_player : VlcMediaPlayer*)
+    fun free_media_player = libvlc_media_player_release(media_player : VlcMediaPlayer*)
     fun set_media_player_media = libvlc_media_player_set_media(media_player : VlcMediaPlayer*, media : VlcMedia*)
     fun can_media_player_pause? = libvlc_media_player_can_pause(media_player : VlcMediaPlayer*) : Bool
 
